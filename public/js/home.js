98c1f11cd21ab5f96c7565e2fd1c16a76dd511f5
Chad Pan <cppan0@gmail.com>
2018-12-18 15:42:54 -0600

    added function to display saved tickers in user-home.html

--- public/js/home.js
+++ public/js/home.js
@@ -14,5 +14,17 @@ function readUserPortfolio(id) {
 
 //takes assetName and currentPrice variables and generate HTML to display - chad to do
 function displayPortfolio(asset, price) {
-
+    const cardDiv = $("<div class='card'>");
+    const bodyDiv = $("<div class='card-body'>");
+    const removeBtn = $("<button class='btn btn-secondary'>Remove</button>");
+    $("#searchResult").append(cardDiv);
+    cardDiv.append(bodyDiv);
+    bodyDiv.append("<h5 class='card-title'>Asset: " + asset + "</h5> <p class='card-text'>Current price: " + price + "</p>");
+    bodyDiv.append(removeBtn);
+    removeBtn.on("click", removeDiv);
 }
+//remove the div once the button is clicked
+function removeDiv(){
+    $(this).parent().parent().remove();
+}
+
